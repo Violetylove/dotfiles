2cfaef4b347d344f59aed6f49eeff9e7229f27ad
Winter <ayanamiviolet@163.com>
2024-01-31 14:04:55 +0800

    litte change

--- scripts/install-win.ps1
+++ scripts/install-win.ps1
@@ -1,33 +1,34 @@
 #!pwsh
 #将git仓库中配置更新到应用
-if (!(Test-Path -Path "~\.glaze-wm"))
+# if (!(Test-Path -Path "~\.glaze-wm"))
+# {
+#     New-Item -ItemType Directory -Path "~\.glaze-wm"
+# }
+if (!(Test-Path -Path "~\.config"))
 {
-    New-Item -ItemType Directory -Path "~\.glaze-wm"
+    New-Item -ItemType Directory -Path "~\.config"
 }
-if (!(Test-Path -Path "~\.config\scoop"))
+if (!(Test-Path -Path "~\AppData\Roaming\"))
 {
-    New-Item -ItemType Directory -Path "~\.config\scoop"
-}
-if (!(Test-Path -Path "~\AppData\Roaming\alacritty"))
-{
-    New-Item -ItemType Directory -Path "~\AppData\Roaming\alacritty"
+    New-Item -ItemType Directory -Path "~\AppData\Roaming"
 }
 if (!(Test-Path -Path "~\Documents\PowerShell"))
 {
     New-Item -ItemType Directory -Path "~\Documents\PowerShell"
 }
-if (!(Test-Path -Path "~\AppData\Roaming\alacritty"))
+if (!(Test-Path -Path "~\AppData\Roaming"))
 {
-    New-Item -ItemType Directory -Path "~\AppData\Roaming\alacritty"
+    New-Item -ItemType Directory -Path "~\AppData\Roaming"
 }
 
 Copy-Item -Recurse -Force ..\idea\.ideavimrc ~\.ideavimrc
+Copy-Item -Recurse -Force ..\git\.gitconfig ~\.gitconfig
 Copy-Item -Recurse -Force ..\vscode\.vscodevimrc ~\.vscodevimrc
-Copy-Item -Recurse -Force ..\windows\GlazeWM\config.yaml ~\.glaze-wm\.config.yaml
-Copy-Item -Recurse -Force ..\windows\scoop/config.json ~\.config\scoop\config.json
+Copy-Item -Recurse -Force ..\windows\GlazeWM\config.yaml ~\.glaze-wm\config.yaml
+Copy-Item -Recurse -Force ..\windows\scoop\config.json ~\.config\scoop\config.json
 Copy-Item -Recurse -Force ..\shell\alacritty ~\AppData\Roaming\alacritty
 Copy-Item -Recurse -Force ..\shell\power_shell\Microsoft.PowerShell_profile.ps1 ~\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
-Copy-Item -Recurse -Force ..\nvim ~\AppData\Roaming\nvim
+Copy-Item -Recurse -Force ..\nvim ~\AppData\Roaming
 
 
 
--- scripts/update-win.ps1
+++ scripts/update-win.ps1
@@ -1,9 +1,11 @@
 #!pwsh
 #将应用配置更新到git仓库中
 
-Copy-Item -Recurse -Force ~\.glaze-wm\.config.yaml ..\windows\GlazeWM\config.yaml
-Copy-Item -Recurse -Force ~\.config\scoop\config.json ..\windows\scoop/config.json
-Copy-Item -Recurse -Force ~\AppData\Roaming\alacritty ..\shell\alacritty ~\AppData\Roaming\alacritty
+Copy-Item -Recurse -Force ~\.glaze-wm\config.yaml ..\windows\GlazeWM\config.yaml
+Copy-Item -Recurse -Force ~\.gitconfig ..\git\.gitconfig
+Copy-Item -Recurse -Force ~\.config\scoop\config.json ..\windows\scoop\config.json
+Copy-Item -Recurse -Force ~\AppData\Roaming\alacritty ..\shell 
 Copy-Item -Recurse -Force ~\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 ..\shell\power_shell\Microsoft.PowerShell_profile.ps1
+
 
 
