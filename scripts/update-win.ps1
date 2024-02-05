792d69ba8e5929801a512dd1b975426b120f18ec
Winter <ayanamiviolet@163.com>
2024-02-06 01:03:49 +0800

    litte change

--- scripts/update-win.ps1
+++ scripts/update-win.ps1
@@ -4,8 +4,7 @@
 Copy-Item -Recurse -Force ~\.glaze-wm\config.yaml ..\windows\GlazeWM\config.yaml
 Copy-Item -Recurse -Force ~\.gitconfig ..\git\.gitconfig
 Copy-Item -Recurse -Force ~\.config\scoop\config.json ..\windows\scoop\config.json
-Copy-Item -Recurse -Force ~\AppData\Roaming\alacritty ..\shell 
+# Copy-Item -Recurse -Force ~\AppData\Roaming\alacritty ..\shell 
 Copy-Item -Recurse -Force ~\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 ..\shell\power_shell\Microsoft.PowerShell_profile.ps1
-
 
 
